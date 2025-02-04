1c7b635e2b8a547abf824c8f9665c2671d9ac42f
code-kay <kay8563@gmail.com>
2025-02-04 11:21:23 +0900

    update

--- bab.py
+++ bab.py
@@ -13,21 +13,20 @@ import re
 from datetime import date, timedelta
 
 kr_holidays = holidays.KR()
-today = date.today() + timedelta(days=1)
+today = date.today()
 
 # 오늘이 공휴일인지 확인
 if today in kr_holidays:
-    print(f"내일은 공휴일입니다. ({kr_holidays.get(today)}) 작업을 중단합니다.")
-
+    print(f"오늘은 공휴일입니다. ({kr_holidays.get(today)}) 작업을 중단합니다.")
 else:
     WEBHOOK_URL = os.getenv('WEBHOOK_URL')
     BLOG_URL = os.getenv('BLOG_URL')
+
     # Chrome 옵션 설정
     chrome_options = Options()
     chrome_options.add_argument("--headless")  # 필요한 경우 헤드리스 모드 사용
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
-
 
     # ChromeDriver 설정
     service = Service(ChromeDriverManager().install())
@@ -40,25 +39,27 @@ else:
 
     # 페이지 로딩 대기
     wait = WebDriverWait(driver, 10)
-    wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'wrap_fit_thumb')))
+    wrap_fit_thumb = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'wrap_fit_thumb')))
+
+    # wrap_fit_thumb 클릭하여 새로운 페이지로 이동
+    wrap_fit_thumb.click()
+
+    # 새 페이지 로딩 대기
+    wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'item_archive_image')))
 
     # 페이지 소스 가져오기
     html = driver.page_source
     soup = BeautifulSoup(html, 'html.parser')
 
-    # wrap_fit_thumb 클래스명으로 되어있는 div 가져오기
-    div = soup.find('div', class_='wrap_fit_thumb')
+    # item_archive_image 클래스를 가진 div 내부의 img 태그 찾기
+    images = soup.select('.item_archive_image img')
+    image_urls = [img['src'] for img in images] if images else []
 
-    # style 속성에서 배경 이미지 URL 추출
-    first_img = None
-    if div and div.get('style'):
-        match = re.search(r'url\("(.+?)"\)', div['style'])
-        if match:
-            first_img = match.group(1)
+    if image_urls:
+        last_img = image_urls[-1]  # 마지막 이미지 URL
+        other_images = image_urls[:-1]  # 마지막 이미지를 제외한 나머지
 
-    if first_img:
-        img_url = first_img
-        print(f"이미지 URL: {img_url}")
+        print(f"이미지 URL 목록: {image_urls}")
 
         # 웹훅으로 전송
         data = {
@@ -80,7 +81,13 @@ else:
                             },
                             {
                                 "type": "Image",
-                                "url": img_url,
+                                "url": last_img,
+                            },
+                            {
+                                "type": "ImageSet",
+                                "images": [
+                                    {"type": "Image", "url": url, "altText": "Image"} for url in other_images
+                                ]
                             }
                         ]
                     }
@@ -92,6 +99,6 @@ else:
         print(f'웹훅 전송 상태 코드: {webhook_response.status_code}')
 
     else:
-        print("첫 번째 이미지를 찾을 수 없습니다.")
+        print("이미지를 찾을 수 없습니다.")
 
     driver.quit()
